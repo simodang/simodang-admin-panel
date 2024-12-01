21736ad7f0555e3d3469e1a0ac0d03f37f92e9b1
Aldi-H <aldihimawan88@gmail.com>
2024-12-01 12:33:43 +0700

    fix: add missing line break in ThemeToggle component for improved readability

--- src/app/ThemeToggle.tsx
+++ src/app/ThemeToggle.tsx
@@ -4,6 +4,7 @@ import { useTheme } from "next-themes";
 
 export function ThemeToggle() {
   const { resolvedTheme, setTheme } = useTheme();
+
   return (
     <button
       className="mt-16 px-4 py-2  dark:text-white text-black dark:bg-black bg-white font-semibold rounded-md"
